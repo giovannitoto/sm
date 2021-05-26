93487791386a56a5392c4d1dc5fdca5eae907148
Emanuele Donà <dona.emanuele97@gmail.com>
2021-05-26 14:45:08 +0200

    Reti di parole sui gruppi trovati con louvain

--- 03_analisi_esplorative.R
+++ 03_analisi_esplorative.R
@@ -11,10 +11,13 @@ library(tidytext)
 library(quanteda)
 library(quanteda.textstats)
 library(quanteda.textmodels)
+library(tidygraph) 
+library(ggraph)
 
 # ---------------------------------------------------------------------------- #
 
 rm(list=ls())
+setwd("C:/Users/Emanuele/Documenti/GitHub/sm")
 load("data/02_tweets_stem.RData")
 
 # ---------------------------------------------------------------------------- #
@@ -85,29 +88,116 @@ g = delete_vertices(g, which(components(g)$membership != 1))
 
 #g = delete_vertices(g, which(degree(g) < 2))
 
-components(g)
+
 
 #Cluster####
 
 #LOUVAIN####
 gr = cluster_louvain(graph = g)
+table(gr$membership)
+#1     2     3     4     5     6     7     8     9    10    11    12    13    14    15 
+#5425    34 11262  2678    20    13   558 10061  1421     8    13     5 10050     6     4 
+
+#------------------------------------------#
+# Gruppo 3
+#------------------------------------------#
+
+X = tt[tt$word %in% gr$names[gr$membership == 3],] %>% cast_sparse(row=id, column=word, value=freq)
+quantile(colSums(X), probs = seq(0.89, 0.99, length = 10))
+X <- X[, colSums(X)>quantile(colSums(X), probs = 0.97)]
+dim(X)  #  24669 11262
+M=t(X) %*% X
+g3 = graph_from_adjacency_matrix(M, weighted=TRUE, mode="undirected",
+                                add.rownames=TRUE)
+# 11262 272971
+g3 = simplify(g3, remove.loops=T) #11262 261709
 #rappresentazione grafica
-library(tidygraph) 
-library(ggraph)
-gr_id = membership(gr)
-V(g)$color = as.numeric(factor(gr_id))
-large = which(degree(g)>quantile(degree(g), .75))
-V(g)$label = NA
-V(g)$label[large] = V(g)$name[large]
-df_gg = as_tbl_graph(g)
-pl = ggraph(df_gg, layout="fr") +
+large = which(degree(g3)>quantile(degree(g3), .9))
+V(g3)$label = NA
+V(g3)$label[large] = V(g3)$name[large]
+df_gg = as_tbl_graph(g3)
+pl3 <- ggraph(df_gg, layout="fr") +
   geom_edge_link(show.legend = FALSE, aes(alpha=weight)) +
-  geom_node_point(aes(color = factor(color)),show.legend = F) +
-  geom_node_label(aes(label=label,colour = factor(color)),show.legend=F) +
+  geom_node_label(aes(label=label,colour = 2),show.legend=F) +
   scale_size(guide="none") +
   theme_graph()
+
 # salvo in file
-ggsave(pl, file="net.pdf", device=cairo_pdf, width=20, height=10)
+ggsave(pl3, file="net3.pdf", device=cairo_pdf, width=20, height=10)
+
+
+#------------------------------------------#
+# Gruppo 8
+#------------------------------------------#
+
+X = tt[tt$word %in% gr$names[gr$membership == 8],] %>% cast_sparse(row=id, column=word, value=freq)
+quantile(colSums(X), probs = seq(0.89, 0.99, length = 10))
+X <- X[, colSums(X)>quantile(colSums(X), probs = 0.97)]
+dim(X) #27777   300
+M=t(X) %*% X
+g8 = graph_from_adjacency_matrix(M, weighted=TRUE, mode="undirected",
+                                 add.rownames=TRUE)
+g8 = simplify(g8, remove.loops=T) 
+large = which(degree(g8)>quantile(degree(g8), .9))
+V(g8)$label = NA
+V(g8)$label[large] = V(g8)$name[large]
+df_gg = as_tbl_graph(g8)
+pl8 <- ggraph(df_gg, layout="fr") +
+  geom_edge_link(show.legend = FALSE, aes(alpha=weight)) +
+  geom_node_label(aes(label=label,colour = 2),show.legend=F) +
+  scale_size(guide="none") +
+  theme_graph()
+
+ggsave(pl8, file="net8.pdf", device=cairo_pdf, width=20, height=10)
+
+#------------------------------------------#
+# Gruppo 13
+#------------------------------------------#
+
+X = tt[tt$word %in% gr$names[gr$membership == 13],] %>% cast_sparse(row=id, column=word, value=freq)
+quantile(colSums(X), probs = seq(0.89, 0.99, length = 10))
+X <- X[, colSums(X)>quantile(colSums(X), probs = 0.97)]
+dim(X)
+M=t(X) %*% X
+g13 = graph_from_adjacency_matrix(M, weighted=TRUE, mode="undirected",
+                                 add.rownames=TRUE)
+g13 = simplify(g13, remove.loops=T) 
+large = which(degree(g13)>quantile(degree(g13), .9))
+V(g13)$label = NA
+V(g13)$label[large] = V(g13)$name[large]
+df_gg = as_tbl_graph(g13)
+pl13 <- ggraph(df_gg, layout="fr") +
+  geom_edge_link(show.legend = FALSE, aes(alpha=weight)) +
+  geom_node_label(aes(label=label,colour = 2),show.legend=F) +
+  scale_size(guide="none") +
+  theme_graph()
+
+ggsave(pl13, file="net13.pdf", device=cairo_pdf, width=20, height=10)
+
+#------------------------------------------#
+# Gruppo 1
+#------------------------------------------#
+
+X = tt[tt$word %in% gr$names[gr$membership == 1],] %>% cast_sparse(row=id, column=word, value=freq)
+X <- X[, colSums(X)>quantile(colSums(X), probs = 0.97)]
+dim(X)
+M=t(X) %*% X
+g1 = graph_from_adjacency_matrix(M, weighted=TRUE, mode="undirected",
+                                  add.rownames=TRUE)
+g1 = simplify(g1, remove.loops=T) 
+large = which(degree(g1)>quantile(degree(g1), .9))
+V(g1)$label = NA
+V(g1)$label[large] = V(g1)$name[large]
+df_gg = as_tbl_graph(g1)
+pl1 <- ggraph(df_gg, layout="fr") +
+  geom_edge_link(show.legend = FALSE, aes(alpha=weight)) +
+  geom_node_label(aes(label=label,colour = 2),show.legend=F) +
+  scale_size(guide="none") +
+  theme_graph()
+
+ggsave(pl1, file="net1.pdf", device=cairo_pdf, width=20, height=10)
+
+# ---------------------------------------------------------------------------- #
 
 
 #altri metodi#####
